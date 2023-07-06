72decb063941b8b904445a4567f067233db28156
prakashsvmx <prakash@minio.io>
2023-07-06 21:50:06 +0530

    minio-js vue-js vite setup

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+import nodePolyfills from 'vite-plugin-node-stdlib-browser'
+
+export default defineConfig({
+  plugins: [
+    nodePolyfills(),
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
